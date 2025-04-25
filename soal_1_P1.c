9e09f8850167d8c30e538e3c3165bbafc616c19d
Imamabd47 <16523202@std.stei.itb.ac.id>
2025-04-25 08:45:24 +0700

    tadi salah copy hehe

--- soal_1_P1.c
+++ soal_1_P1.c
@@ -24,34 +24,34 @@
  
      //Validasi Kode terdiri dari 10 Karakter
      if (strlen(Kode)!=10){
-         printf("Kode tidak valid ");
+         printf("Kode tidak valid");
          return 0;}
  
      for (i=0;i<10;i++){
          Index = Kode[i];
          
          if ((Index<48)||(57<Index&&Index<65)||(Index>90)){ //Validasi hanya berisi Huruf Besar(A-Z) dan Angka(0-9)
-             printf("Kode tidak valid ");
+             printf("Kode tidak valid");
              return 0;}
          if (47<Index&&Index<58){
              sum_angka += 1;
              Arr_angka[Index-48] += 1;
-             if (Arr_angka[Index-48]>1){
-                 printf("Kode tidak valid ");
+             if (Arr_angka[Index-48]>2){
+                 printf("Kode tidak valid");
                  return 0;
                  }
              }
          else {
              sum_huruf += 1;
              Arr_huruf[Index-65] += 1;
-             if (Arr_huruf[Index-65]>1){
-                 printf("Kode tidak valid ");
+             if (Arr_huruf[Index-65]>2){
+                 printf("Kode tidak valid");
                  return 0;
              }
          }
      }
      if ((sum_angka<2)||(sum_huruf<2)){//Validasi minimal 2 huruf dan angka
-         printf("Kode tidak valid ");
+         printf("Kode tidak valid");
          return 0;
      }
      //Lulus Aturan Validasi - Kode Valid
